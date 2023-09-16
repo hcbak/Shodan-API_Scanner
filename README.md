44e664424a9293e96502ce251614fb02c878ad3a
hcbak <hc.bak@outlook.com>
2023-09-16 10:54:29 +0900

    기타 기능 추가

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Shodan-API_Scanner
 이 프로젝트의 목적은 쇼단으로 서버를 스캔하여 문서화하는 업무를 자동화하는 것입니다.
 
+## 사용 방법
 1. 검색 할 리스트를 Search_List.txt에 입력
 2. Shodan API Key를 API_Key.txt에 입력
+3. Shodan ID & PW를 Account.txt에 입력
 3. 스크립트 실행
-4. 검색 결과를 json파일로 저장
-5. 필요한 내용만으로 Excel 파일 출력
+
+## 결과
+1. 검색 결과를 json파일로 저장
+2. 특정 정보(server) 노출 시 결과 페이지 스크린샷 저장
+3. 필요한 내용만으로 Excel 파일 출력
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import json
+import time
+import asyncio
 from datetime import datetime
 
 def system_alert(icon = " ", word = ""):
@@ -20,21 +22,28 @@ def search_shodan(api, S_list):
     for i in S_list:
         try:
             results.append(api.host(i))
-        except:
-            results.append("No Results")
+        except Exception as e:
+            try:
+                system_alert("!", i + " Error!! " + str(e))
+                system_alert(" ", "retry...")
+                time.sleep(3)
+                results.append(api.host(i))
+            except Exception as e:
+                system_alert("!", i + " Error!! pass this IP...")
+                results.append("No Results")
         system_alert("+", i)
     return results
 
-def result_to_json(name, data):
+def result_to_json(name, data, DirName):
     try:
         os.makedirs("result")
         system_alert("!", "Dir Ready.")
     except:
         system_alert("+", "Dir Ready.")
-    DirName = datetime.now().strftime("%Y%m%d_%H%M%S%f")
     os.makedirs("result/" + DirName)
+    os.makedirs("result/" + DirName + "/json")
     for i, j in zip(name, data):
-        with open("result/" + DirName + "/" + i + ".json", "w") as f:
+        with open("result/" + DirName + "/json/" + i + ".json", "w") as f:
             json.dump(j, f, indent = 4)
     system_alert("+", "Result File Saved.")
 
@@ -74,7 +83,7 @@ def make_table(data, index):
                     frame["ip_str"].append(j["ip_str"])
                     frame["port"].append(j["port"])
                     frame["module"].append(j["_shodan"]["module"])
-                    frame["server"].append("")
+                    frame["server"].append(None)
                     frame["isp"].append(j["isp"])
                     frame["city"].append(j["location"]["city"])
                     frame["timestamp"].append(j["timestamp"])
@@ -83,27 +92,63 @@ def make_table(data, index):
             system_alert("!", i2 + " Error")
             system_alert("!", e)
             frame["ip_str"].append(i2)
-            frame["port"].append("")
-            frame["module"].append("")
-            frame["server"].append("")
-            frame["isp"].append("")
-            frame["city"].append("")
-            frame["timestamp"].append("")
+            frame["port"].append(None)
+            frame["module"].append(None)
+            frame["server"].append(None)
+            frame["isp"].append(None)
+            frame["city"].append(None)
+            frame["timestamp"].append(None)
                 
-    print(frame)
+            
     df = pd.DataFrame(frame)
     df = df.set_index("ip_str", append = True).swaplevel(1, 0)
 
     return df
 
+# 
+async def async_master(sList, options, exposedDir, Account):
+    futures = [search_tool(i, options, exposedDir, Account) for i in sList]
+    await asyncio.gather(*futures)
+    
+async def search_tool(url, options, exposedDir, Account):
+    system_alert(" ", "Start " + url + " screen capture...")
+    driver = webdriver.Edge(options = options)
+    driver.get("https://account.shodan.io/login")
+    driver.find_element(By.NAME, 'username').send_keys(Account[0])
+    driver.find_element(By.NAME, 'password').send_keys(Account[1])
+    driver.find_element(By.XPATH, '/html/body/div[2]/main/div/div/div/div[1]/form/div[3]/input').click()
+
+    driver.get("https://www.shodan.io/host/" + url)
+    await asyncio.sleep(3)
+    original_size = driver.get_window_size()
+    required_width = driver.execute_script('return document.body.parentNode.scrollWidth')
+    required_height = driver.execute_script('return document.body.parentNode.scrollHeight')
+    driver.set_window_size(required_width, required_height)
+    driver.find_element(By.TAG_NAME, 'body').screenshot(exposedDir + "/"+ url +".png")
+    driver.quit()
+    system_alert("+", "" + url + " capture is done!")
+        
+    
+    
+
 # main
 if __name__ == "__main__":
+
+    DirName = datetime.now().strftime("%Y%m%d_%H%M%S%f")
+
+    print("\n\n\tShodan 노출 검색 자동화 스크립트\n\tv0.0.1β\n\n\t문의: hcbak@sk.com\n\n")
+    
     # import module
     while True:
         try:
             import shodan
             import pandas as pd
             import openpyxl
+            from selenium import webdriver
+            from selenium.webdriver.edge.options import Options
+            from selenium.webdriver.common.by import By
+            import edgedriver_autoinstaller
+            autodriver = edgedriver_autoinstaller.install()
             system_alert("+", "imported all module.")
             break
         except Exception as e:
@@ -111,6 +156,8 @@ if __name__ == "__main__":
             pip_install("shodan")
             pip_install("pandas")
             pip_install("openpyxl")
+            pip_install("selenium")
+            pip_install("edgedriver_autoinstaller")
 
     # get API Key
     try:
@@ -118,7 +165,7 @@ if __name__ == "__main__":
         with open("API_Key.txt", "r") as f:
             ShodanApiKey = f.readline()
         api = shodan.Shodan(ShodanApiKey)
-        system_alert("+", "Connect API.")
+        system_alert("+", "Connected API.")
     except Exception as e:
         system_alert("-", e)
         system_alert("!", "API Key Error.")
@@ -130,18 +177,43 @@ if __name__ == "__main__":
         with open("Search_List.txt", "r") as f:
             for i in f:
                 SearchList.append(i.strip("\n"))
-
     except Exception as e:
         system_alert("-", e)
+        system_alert("!", "Search List Error.")
+
+    # get Account
+    Account = []
+    try:
+        system_alert(" ", "get Account.")
+        with open("Account.txt", "r") as f:
+            for i in f:
+                Account.append(i.strip("\n"))
+        system_alert("+", "read Account.")
+    except Exception as e:
+        system_alert("-", e)
+        system_alert("!", "Account Error.")
 
     results = search_shodan(api, SearchList)
 
-    result_to_json(SearchList, results)
+    result_to_json(SearchList, results, DirName)
 
     df = make_table(results, SearchList)
-    df.to_excel("test.xlsx")
+    df.to_excel("result/" + DirName + "/result.xlsx")
+
+    exposedDir = "result/" + DirName + "/exposed"
+    os.makedirs(exposedDir)
     
+    serverList = df["server"].dropna()
+    serverListIP = [i[0] for i in serverList.index]
 
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--inprivate")
+    options.add_argument("--window-size= 1920, 1080")
 
+    asyncio.run(async_master(serverListIP, options, exposedDir, Account))
+
+    system_alert("+", "End Script.")
+    os.startfile("result\\" + DirName)
 
 os.system("pause")
